6724843dbf32ad49fcf96b77e0f2d3280a8a9bf4
11mad11 <mad.21@live.fr>
2024-07-17 19:48:07 +0000

    add fix for inquirer

--- src/commands/_helper.ts
+++ src/commands/_helper.ts
@@ -1,6 +1,6 @@
 import { ServerChannel } from "ssh2";
 import { CommandRegister } from "../command";
-import { input } from '@inquirer/prompts';
+import * as inquirerO from '@inquirer/prompts';
 
 export function cmd(cmd: CommandRegister) {
     return cmd;
@@ -14,16 +14,33 @@ export function getInquirerContext(channel: ServerChannel) {
 }
 
 export const inquirer = {
-    input(...args: Parameters<typeof input>): ReturnType<typeof input> {
-        const prompt = input(...args);
-        args[1]?.input?.on("close", () => {
-            prompt.cancel();
-        })
-        return prompt;
-    }
+    input: fix(inquirerO.input),
+    select: fix(inquirerO.select),
+    checkbox: fix(inquirerO.checkbox),
+    confirm: fix(inquirerO.confirm),
+    editor: fix(inquirerO.editor),
+    expand: fix(inquirerO.expand),
+    number: fix(inquirerO.number),
+    password: fix(inquirerO.password),
+    rawlist: fix(inquirerO.rawlist),
 }
 
+function fix<A extends [any, Parameters<typeof inquirerO.input>[1]], F extends (...args: A) => ReturnType<import("@inquirer/type").Prompt<any, any>>>(fn: F): F {
+    return ((...args: A) => {
+        const prompt = fn(...args);
+        args[1]?.input?.on("close", () => {
+            prompt.cancel();//Without this, the server process can't exit if a user close stream mid-prompt
+        })
+        return prompt;
+    }) as F
+}
 
+/**
+ * Inquirer close the stream after each prompt
+ * @param obj 
+ * @param name 
+ * @returns 
+ */
 function createProxy<T extends object>(obj: T, name: string): T {
     return new Proxy(obj, {
         get(target, property, receiver) {
